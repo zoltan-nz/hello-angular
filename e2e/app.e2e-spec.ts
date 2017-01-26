79725e027fd123e3e8c2628a2dc5b5a4cf3e6951
Zoltan <zdebre@gmail.com>
2017-01-26 22:07:00 +1300

    Reset it with angular-cli

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,15 +1,14 @@
-import { browser, element, by } from 'protractor';
+import { HelloAngularPage } from './app.po';
 
-describe('QuickStart E2E Tests', function () {
+describe('hello-angular App', function() {
+  let page: HelloAngularPage;
 
-  let expectedMsg = 'Hello Angular';
-
-  beforeEach(function () {
-    browser.get('');
+  beforeEach(() => {
+    page = new HelloAngularPage();
   });
 
-  it('should display: ' + expectedMsg, function () {
-    expect(element(by.css('h1')).getText()).toEqual(expectedMsg);
+  it('should display message saying app works', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('app works!');
   });
-
 });
