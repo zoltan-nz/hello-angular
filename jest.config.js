e45528799018f87a8b29bcb245d61056ab339e93
Zoltan <zdebre@gmail.com>
2019-04-30 00:26:00 +1200

    Migrate to Jest

---
+++ jest.config.js
@@ -0,0 +1,39 @@
+// https://github.com/thymikee/jest-preset-angular#brief-explanation-of-config
+module.exports = {
+  globals: {
+    'ts-jest': {
+      diagnostics: {
+        warnOnly: true
+      },
+      tsConfig: '<rootDir>/src/tsconfig.spec.json',
+      stringifyContentPathRegex: '\\.html$',
+      astTransformers: [require.resolve('jest-preset-angular/InlineHtmlStripStylesTransformer')],
+    },
+  },
+  transform: {
+    '^.+\\.(ts|js|html)$': 'ts-jest',
+  },
+  testEnvironment: 'jest-environment-jsdom-thirteen',
+  moduleFileExtensions: ['ts', 'html', 'js', 'json'],
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+    '^app/(.*)$': '<rootDir>/src/app/$1',
+    '^assets/(.*)$': '<rootDir>/src/assets/$1',
+    '^environments/(.*)$': '<rootDir>/src/environments/$1',
+
+    '@app/(.*)': '<rootDir>/src/app/$1',
+    '@assets/(.*)': '<rootDir>/src/assets/$1',
+    '@core/(.*)': '<rootDir>/src/app/core/$1',
+    '@env': '<rootDir>/src/environments/environment',
+    '@src/(.*)': '<rootDir>/src/src/$1',
+    '@state/(.*)': '<rootDir>/src/app/state/$1'
+  },
+  transformIgnorePatterns: ['node_modules/(?!@ngrx)', 'node_modules/(?!(jest-test))'],
+  snapshotSerializers: [
+    'jest-preset-angular/AngularSnapshotSerializer.js',
+    'jest-preset-angular/HTMLCommentSerializer.js',
+  ],
+  roots: ['src'],
+  setupFilesAfterEnv: ['<rootDir>/src/setup-jest.ts'],
+};
+
