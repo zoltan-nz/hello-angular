9f5ff1ddd6d763a31530eaa835d87ff3174c66d4
Zoltan <zdebre@gmail.com>
2019-09-05 13:53:00 +1200

    Update e2e test to check the Home Page

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('hello-angular app is running!');
+    expect(page.getTitleText()).toEqual('Home Page');
   });
 
   afterEach(async () => {
