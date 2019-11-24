b6a487c4114401f65347575b12f9f66b52f3fda7
Zoltan <zdebre@gmail.com>
2019-11-24 19:44:00 +1300

    Generate Admin module

---
+++ src/app/admin/admin.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AdminRoutingModule } from './admin-routing.module';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule, AdminRoutingModule],
+})
+export class AdminModule {}
