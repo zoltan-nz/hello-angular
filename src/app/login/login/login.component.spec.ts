97a3a2eec230915957e253be20349fc3862090b9
Zoltan <zdebre@gmail.com>
2019-12-05 18:39:00 +1300

    Add Login form using bootstrap 4

--- src/app/login/login/login.component.spec.ts
+++ src/app/login/login/login.component.spec.ts
@@ -1,14 +1,24 @@
-import { Spectator, createComponentFactory } from '@ngneat/spectator';
+import { Spectator, createComponentFactory, byText, byTestId, byLabel } from '@ngneat/spectator';
 
 import { LoginComponent } from './login.component';
 
 describe('LoginComponent', () => {
   let spectator: Spectator<LoginComponent>;
+  let component: LoginComponent;
   const createComponent = createComponentFactory(LoginComponent);
 
-  it('should create', () => {
+  beforeEach(() => {
     spectator = createComponent();
+    component = spectator.component;
+  });
 
-    expect(spectator.component).toBeTruthy();
+  it('should create', () => {
+    expect(component).toBeDefined();
+  });
+
+  test('Login Form', () => {
+    expect(spectator.query('[data-test-header]')).toContainText('Login');
+    expect(spectator.query(byLabel('Username'))).toBeDefined();
+    expect(spectator.query(byLabel('Password'))).toBeDefined();
   });
 });
