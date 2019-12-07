4ac4cc42363360f18d9bb459b199de1604ae311d
Zoltan <zdebre@gmail.com>
2019-12-08 10:54:00 +1300

    Use touched property on form validators to show error after changes

--- src/app/login/login/login.component.ts
+++ src/app/login/login/login.component.ts
@@ -7,8 +7,8 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
-  username = new FormControl('', { validators: Validators.required, updateOn: 'blur' });
-  password = new FormControl('', { validators: Validators.required, updateOn: 'blur' });
+  username = new FormControl('', { validators: Validators.required, updateOn: 'change' });
+  password = new FormControl('', { validators: Validators.required, updateOn: 'change' });
 
   loginForm = new FormGroup({
     username: this.username,
