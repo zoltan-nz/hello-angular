a49dd634c6835b6a581cff15d8e83f3e96daec28
Zoltan <zdebre@gmail.com>
2020-01-14 17:44:00 +1300

    Add authentication guard to protected route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,21 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HomePageComponent } from './home-page/home-page.component';
-import { StoreAppComponent } from './store-app/store-app.component';
+import { AuthGuard } from './_helpers/auth.guard';
 import { AdminComponent } from './admin/admin/admin.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { LoginComponent } from './login/components/login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { StoreAppComponent } from './store-app/store-app.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: HomePageComponent },
   { path: 'store', component: StoreAppComponent },
-  { path: 'admin', component: AdminComponent },
+  { path: 'admin', component: AdminComponent, canActivate: [AuthGuard] },
   { path: 'login', component: LoginComponent },
   { path: '**', component: PageNotFoundComponent },
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { enableTracing: true })],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
